3d5e02b1ccc0b21ce32761673ea9e4dd16c39c1a
jairomelo <jairoantoniomelo@gmail.com>
2025-06-16 14:43:08 -0700

    feat: refactor working_dataframes function to improve logging and structure

--- project_code/datacleaning.py
+++ project_code/datacleaning.py
@@ -1,15 +1,42 @@
 import pandas as pd
 from pathlib import Path
 
+from helpers.LogerHandler import setup_logger
+
 from helpers.ColumnManager import ColumnManager
 
+def setup_test_logger(test_name):
+    """
+    Set up a logger for a specific test.
+    """
+    logger = setup_logger(test_name)
+    return logger
 
-data = ["bautismos", "entierros", "matrimonios"]
-filesorigins = [Path("data/raw") / f"{d}.csv" for d in data]
-filesmappings = [Path("data/mappings") / f"{d}Mapping.json" for d in data]
+logger = setup_test_logger("datacleaning")
 
-for data, fileorigin, filemapping in zip(data, filesorigins, filesmappings):
-    column_manager = ColumnManager()
-    dataset = column_manager.harmonize_columns(fileorigin, filemapping)
-    dataset = column_manager.return_useful_columns(dataset)
-    print(f"Dataset for {data}:\n{dataset.head()}\n")
+def working_dataframes():
+    """
+    Returns a list of DataFrames ready for cleaning and harmonization.
+    """
+
+    data = ["bautismos", "entierros", "matrimonios"]
+    filesorigins = [Path("data/raw") / f"{d}.csv" for d in data]
+    filesmappings = [Path("data/mappings") / f"{d}Mapping.json" for d in data]
+
+    dataframes = []
+
+    for data, fileorigin, filemapping in zip(data, filesorigins, filesmappings):
+        column_manager = ColumnManager()
+        dataset = column_manager.harmonize_columns(fileorigin, filemapping)
+        dataset = column_manager.return_useful_columns(dataset)
+        logger.info(f"Dataset {data} columns: {dataset.columns.tolist()}")
+        dataframes.append(dataset)
+
+    return dataframes
+
+if __name__ == "__main__":
+    logger.info("Starting data cleaning process")
+    dataframes = working_dataframes()
+    for df in dataframes:
+        logger.info(f"DataFrame shape: {df.shape}")
+        logger.info(f"DataFrame columns: {df.columns.tolist()}")
