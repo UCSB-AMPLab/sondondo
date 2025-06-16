e3d778121ec2607653ae3fc026f2f3c5a311f85a
jairomelo <jairoantoniomelo@gmail.com>
2025-06-16 16:10:37 -0700

    new code order

--- project_code/datacleaning.py
+++ project_code/datacleaning.py
@@ -5,6 +5,8 @@ from utils.LoggerHandler import setup_logger
 
 from utils.ColumnManager import ColumnManager
 
+from actions.normalizers.DatesNormalizer import DateNormalizer
+
 def setup_test_logger(test_name):
     """
     Set up a logger for a specific test.
@@ -13,3 +15,25 @@ def setup_test_logger(test_name):
     return logger
 
 logger = setup_test_logger("datacleaning")
+
+dummyPandasSerieswithDates = pd.Series([
+    "2023-01-01",
+    "2022-12-31",
+    "2021-06-15",
+    "invalid_date",
+    "2020-02-29"
+])
+
+def test_date_normalizer():
+    """
+    Test the DateNormalizer class.
+    """
+    logger.info("Starting test_date_normalizer")
+    
+    normalizer = DateNormalizer(dummyPandasSerieswithDates)
+    normalized_dates = normalizer.normalize()
+    
+    logger.info(f"Normalized dates: {normalized_dates}")
+
+
+test_date_normalizer()
